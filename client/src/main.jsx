8df694e1419c81952300a7e23f6a98833c09502c
NicoGER1 <nico3g@hotmail.fr>
2024-04-10 16:39:04 +0200

    Import background image for the new page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import Map from "./pages/Map";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/Map",
+    element: <Map />,
   },
 ]);
 
---
+++ client/src/pages/Map.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import "../styles/Map.scss";
+
+function Map() {
+  return (
+    <div className="Bg-container">
+      <Link to="/">Home</Link>
+    </div>
+  );
+}
+
+export default Map;
