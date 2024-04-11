1698a06c2db2ac620f788683c70e2a8236b2d46e
unknown <sebastien.creuze@live.fr>
2024-04-11 17:52:10 +0200

    update img and background

--- client/src/pages/Map.jsx
+++ client/src/pages/Map.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import "../styles/Map.scss";
+import Banner from "../components/Banner";
 
 function Map() {
   return (
     <div className="Bg-container">
       <Link to="/">Home</Link>
+      <Banner />
     </div>
   );
 }
