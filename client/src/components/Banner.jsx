1a39a1a1ca9eafb65e84740731801b15ec922a47
unknown <sebastien.creuze@live.fr>
2024-04-17 09:54:26 +0200

    first modal test

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import hogwarts from "../assets/hogwarts.png";
 import gryffindor from "../assets/gryffindor.png";
 import slytherin from "../assets/slytherin.png";
@@ -9,33 +10,244 @@ import spells from "../assets/spells.png";
 import "../styles/Banner.scss";
 
 function Banner() {
+  const [toggle1, setToggle1] = useState(false);
+  const [toggle2, setToggle2] = useState(false);
+  const [toggle3, setToggle3] = useState(false);
+  const [toggle4, setToggle4] = useState(false);
+  const [toggle5, setToggle5] = useState(false);
+  const [toggle6, setToggle6] = useState(false);
+  const [toggle7, setToggle7] = useState(false);
+
   return (
     <>
       <section className="first-line">
         <article className="castle">
-          <img src={hogwarts} alt="Hogwarts Castle" />
+          {toggle1 ? (
+            <div className="modal">
+              <h1>Info</h1>
+              <ul>
+                <li>Name</li>
+                <li>Lastname</li>
+                <li>Age</li>
+              </ul>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Placeat harum voluptatibus illo repellat odit eos unde rem
+                facere dolorem. Consequuntur.
+              </p>
+              <button
+                type="button"
+                className="fermer"
+                onClick={() => setToggle1(false)}
+              >
+                &#10005;
+              </button>
+            </div>
+          ) : (
+            <button
+              type="button"
+              className="modal-click"
+              onClick={() => setToggle1(true)}
+            >
+              <img src={hogwarts} alt="Hogwarts Castle" />
+            </button>
+          )}
         </article>
         <article className="gryffindor">
-          <img src={gryffindor} alt="Gryffindor Shield" />
+          {toggle2 ? (
+            <div className="modal">
+              <h1>Info</h1>
+              <ul>
+                <li>Name</li>
+                <li>Lastname</li>
+                <li>Age</li>
+              </ul>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Placeat harum voluptatibus illo repellat odit eos unde rem
+                facere dolorem. Consequuntur.
+              </p>
+              <button
+                type="button"
+                className="fermer"
+                onClick={() => setToggle2(false)}
+              >
+                &#10005;
+              </button>
+            </div>
+          ) : (
+            <button
+              type="button"
+              className="modal-click"
+              onClick={() => setToggle2(true)}
+            >
+              <img src={gryffindor} alt="Gryffindor Shield" />
+            </button>
+          )}
         </article>
       </section>
       <section className="second-line">
         <article className="slytherin">
-          <img src={slytherin} alt="Slytherin Shield" />
+          {toggle3 ? (
+            <div className="modal">
+              <h1>Info</h1>
+              <ul>
+                <li>Name</li>
+                <li>Lastname</li>
+                <li>Age</li>
+              </ul>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Placeat harum voluptatibus illo repellat odit eos unde rem
+                facere dolorem. Consequuntur.
+              </p>
+              <button
+                type="button"
+                className="fermer"
+                onClick={() => setToggle3(false)}
+              >
+                &#10005;
+              </button>
+            </div>
+          ) : (
+            <button
+              type="button"
+              className="modal-click"
+              onClick={() => setToggle3(true)}
+            >
+              <img src={slytherin} alt="Slytherin Shield" />
+            </button>
+          )}
         </article>
         <article className="hufflepuff">
-          <img src={hufflepuff} alt="Hufflepuff Shield" />
+          {toggle4 ? (
+            <div className="modal">
+              <h1>Info</h1>
+              <ul>
+                <li>Name</li>
+                <li>Lastname</li>
+                <li>Age</li>
+              </ul>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Placeat harum voluptatibus illo repellat odit eos unde rem
+                facere dolorem. Consequuntur.
+              </p>
+              <button
+                type="button"
+                className="fermer"
+                onClick={() => setToggle4(false)}
+              >
+                &#10005;
+              </button>
+            </div>
+          ) : (
+            <button
+              type="button"
+              className="modal-click"
+              onClick={() => setToggle4(true)}
+            >
+              <img src={hufflepuff} alt="Hufflepuff Shield" />
+            </button>
+          )}
         </article>
         <article className="ravenclaw">
-          <img src={ravenclaw} alt=" Ravenclaw Shield" />
+          {toggle5 ? (
+            <div className="modal">
+              <h1>Info</h1>
+              <ul>
+                <li>Name</li>
+                <li>Lastname</li>
+                <li>Age</li>
+              </ul>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Placeat harum voluptatibus illo repellat odit eos unde rem
+                facere dolorem. Consequuntur.
+              </p>
+              <button
+                type="button"
+                className="fermer"
+                onClick={() => setToggle5(false)}
+              >
+                &#10005;
+              </button>
+            </div>
+          ) : (
+            <button
+              type="button"
+              className="modal-click"
+              onClick={() => setToggle5(true)}
+            >
+              <img src={ravenclaw} alt=" Ravenclaw Shield" />
+            </button>
+          )}
         </article>
       </section>
       <section className="third-line">
         <article className="elixirs">
-          <img src={elixirs} alt="elixirs-informations" />
+          {toggle6 ? (
+            <div className="modal">
+              <h1>Info</h1>
+              <ul>
+                <li>Name</li>
+                <li>Lastname</li>
+                <li>Age</li>
+              </ul>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Placeat harum voluptatibus illo repellat odit eos unde rem
+                facere dolorem. Consequuntur.
+              </p>
+              <button
+                type="button"
+                className="fermer"
+                onClick={() => setToggle6(false)}
+              >
+                &#10005;
+              </button>
+            </div>
+          ) : (
+            <button
+              type="button"
+              className="modal-click"
+              onClick={() => setToggle6(true)}
+            >
+              <img src={elixirs} alt="elixirs-informations" />
+            </button>
+          )}
         </article>
         <article className="spells">
-          <img src={spells} alt="spells-informations" />
+          {toggle7 ? (
+            <div className="modal">
+              <h1>Info</h1>
+              <ul>
+                <li>Name</li>
+                <li>Lastname</li>
+                <li>Age</li>
+              </ul>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Placeat harum voluptatibus illo repellat odit eos unde rem
+                facere dolorem. Consequuntur.
+              </p>
+              <button
+                type="button"
+                className="fermer"
+                onClick={() => setToggle7(false)}
+              >
+                &#10005;
+              </button>
+            </div>
+          ) : (
+            <button
+              className="modal-click"
+              onClick={() => setToggle7(true)}
+              type="button"
+            >
+              <img src={spells} alt="spells-informations" />
+            </button>
+          )}
         </article>
       </section>
     </>
