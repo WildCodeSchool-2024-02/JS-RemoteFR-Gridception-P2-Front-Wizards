f7a00820c66da42cb5954228eedc69cb415b9e34
NicoGER1 <nico3g@hotmail.fr>
2024-04-24 20:59:53 +0200

    add feature to creator

--- client/src/pages/Creator.jsx
+++ client/src/pages/Creator.jsx
@@ -11,7 +11,7 @@ function Creator() {
       <NavBar />
       <h1 className="title-creator">The Fronts Wizards Team</h1>
       <div className="article-container">
-        <article className="creator-details">
+        <article className="creator-amel">
           <img src={Amel} alt="Portrait d'Amel" />
           <h2 className="name">Amel</h2> <h3 className="h3-creator">House: </h3>{" "}
           <p className="text-creator">Gryffindor</p>
@@ -19,7 +19,7 @@ function Creator() {
           <p className="text-creator">Expelliarmus</p>
         </article>
 
-        <article className="creator-details">
+        <article className="creator-nico">
           <img src={Nico} alt="Portrait de Nico" />
           <h2 className="name">Nicolas</h2>{" "}
           <h3 className="h3-creator">House</h3>
@@ -28,7 +28,7 @@ function Creator() {
           <p className="text-creator">Sectumsempra</p>
         </article>
 
-        <article className="creator-details">
+        <article className="creator-rachel">
           <img src={Rachel} alt="Portrait de Rachel" />
           <h2 className="name">Rachel</h2> <h3 className="h3-creator">House</h3>
           <p className="text-creator">Hufflepuff</p>
@@ -36,7 +36,7 @@ function Creator() {
           <p className="text-creator">Lorem ipsum dolor sit amet.</p>
         </article>
 
-        <article className="creator-details">
+        <article className="creator-seb">
           <img src={Seb} alt="Portrait de Seb" />
           <h2 className="name">Sébastien</h2>{" "}
           <h3 className="h3-creator">House</h3>
@@ -47,7 +47,7 @@ function Creator() {
       </div>
       <footer className="footer">
         <img src={github} alt="logo github" />
-        <p>Created with Magic</p>
+        <p>Created with Magic by the Front Wizards</p>
       </footer>
     </div>
   );
