f8a9b49ecf4f24aa124ec4d04e9d64776a4ab13d
unknown <sebastien.creuze@live.fr>
2024-04-25 17:02:18 +0200

    finished the cursor

--- client/src/pages/Map.jsx
+++ client/src/pages/Map.jsx
@@ -1,9 +1,11 @@
 import Banner from "../components/Banner";
 import NavBar from "../components/NavBar";
+import Test1 from "../components/Cursor";
 
 function Map() {
   return (
     <div className="Bg-container">
+      <Test1 />
       <NavBar />
       <Banner />
     </div>
