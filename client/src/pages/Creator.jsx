a21121e4e958f1fe4a7d194fda8f0f9157e85d69
NicoGER1 <nico3g@hotmail.fr>
2024-04-29 15:17:43 +0200

    Refacto creator page with external dat.js

--- client/src/pages/Creator.jsx
+++ client/src/pages/Creator.jsx
@@ -1,13 +1,10 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import NavBar from "../components/NavBar";
-import Amel from "../assets/amelAvatar.png";
-import Nico from "../assets/nicoAvatar.png";
-import Rachel from "../assets/rachelAvatar.png";
-import Seb from "../assets/sebAvatar.png";
 import github from "../assets/github.png";
-
 import goldenSnitch from "../assets/goldenSnitch.png";
+
+import creatorCards from "../services/DataCreator";
 
 function Creator() {
   const [toggleGolden, setToggleGolden] = useState(false);
@@ -16,39 +13,16 @@ function Creator() {
       <NavBar />
       <h1 className="title-creator">The Fronts Wizards Team</h1>
       <div className="article-container">
-        <article className="creator-amel">
-          <img src={Amel} alt="Portrait d'Amel" />
-          <h2 className="name">Amel</h2> <h3 className="h3-creator">House: </h3>{" "}
-          <p className="text-creator">Gryffindor</p>
-          <h3 className="h3-creator">Favorite spell</h3>
-          <p className="text-creator">Expelliarmus</p>
-        </article>
-
-        <article className="creator-nico">
-          <img src={Nico} alt="Portrait de Nico" />
-          <h2 className="name">Nicolas</h2>{" "}
-          <h3 className="h3-creator">House</h3>
-          <p className="text-creator"> Slytherin</p>
-          <h3 className="h3-creator">Favorite spell</h3>
-          <p className="text-creator">Sectumsempra</p>
-        </article>
-
-        <article className="creator-rachel">
-          <img src={Rachel} alt="Portrait de Rachel" />
-          <h2 className="name">Rachel</h2> <h3 className="h3-creator">House</h3>
-          <p className="text-creator">Hufflepuff</p>
-          <h3 className="h3-creator">Favorite spell</h3>
-          <p className="text-creator">Lorem ipsum dolor sit amet.</p>
-        </article>
-
-        <article className="creator-seb">
-          <img src={Seb} alt="Portrait de Seb" />
-          <h2 className="name">Sébastien</h2>{" "}
-          <h3 className="h3-creator">House</h3>
-          <p className="text-creator">Slytherin</p>
-          <h3 className="h3-creator">Favorite spell</h3>
-          <p className="text-creator">Obliviate</p>
-        </article>
+        {creatorCards.map((element) => (
+          <article key={element.name} className={element.class}>
+            <img src={element.img} alt={element.name} />
+            <h2 className="name">{element.name}</h2>
+            <h3 className="h3-creator">House</h3>
+            <p className="text-creator">{element.house}</p>
+            <h3 className="h3-creator">Favorite spell</h3>
+            <p className="text-creator">{element.favoriteSpell}</p>
+          </article>
+        ))}
       </div>
       <footer className="footer">
         {toggleGolden ? (
